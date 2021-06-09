4c3f55ceeca382d48c280345ca59146d8e06d02f
Alex Mourtos <amourtos@gmail.com>
2021-06-09 14:13:07 -0400

    small edit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio-site">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
